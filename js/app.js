d4cfec4be928dd3e1902296ffc55699421e9bbe4
Zahra6653 <59842591+Zahra6653@users.noreply.github.com>
2020-06-09 21:42:30 +0530

    change font style , background-color

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
 function myFunction() {
 	
-	var x = document.getElementById("myText").value;
-	var y = confirm("Is "+ x + " your name ? ");
+	let x = document.getElementById("myText").value;
+	let y = confirm("Is "+ x + " your name ? ");
 	if(y!=null)
 	{
 		alert(x + ", Welcome to Digi-Safari Git Workshop! ");
 	}
 	}
+function isleapyear()
+{
+	let year= document.getElementById("year").value; 
+    let x = (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
+	if(x===true)
+		alert(year + " is a leap year....");
+	else
+		alert(year + " is not a leap year.....");
+}
